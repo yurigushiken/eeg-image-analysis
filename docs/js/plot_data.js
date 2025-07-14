70d7758d9aab177a7e37d41ade9a691172a98454
yurigushiken <yurigushiken2@gmail.com>
2025-07-13 21:28:36 -0400

    feat: add 'landing on 6' analysis and plots

--- docs/js/plot_data.js
+++ docs/js/plot_data.js
@@ -288,6 +288,42 @@ const plotData = [
         image: 'images/group_n1_contrast_landing_on_5_increasing_acc=1.png',
         scriptUrl: 'https://github.com/yurigushiken/eeg-image-analysis/blob/main/eeg_acc%3D1/code/03_generate_n1_contrast_landing_on_5_increasing_acc%3D1.py'
     },
+    {
+        category: 'Topomaps (ACC=1)',
+        subcategory: 'Landing on 6 (Any Preceding)',
+        dataset: 'ACC=1',
+        name: 'P1',
+        id: 'acc1_p1_land6_any',
+        image: 'images/group_p1_contrast_landing_on_6_any_preceding_acc=1.png',
+        scriptUrl: 'https://github.com/yurigushiken/eeg-image-analysis/blob/main/eeg_acc%3D1/code/03_generate_p1_contrast_landing_on_6_any_preceding_acc%3D1.py'
+    },
+    {
+        category: 'Topomaps (ACC=1)',
+        subcategory: 'Landing on 6 (Any Preceding)',
+        dataset: 'ACC=1',
+        name: 'N1',
+        id: 'acc1_n1_land6_any',
+        image: 'images/group_n1_contrast_landing_on_6_any_preceding_acc=1.png',
+        scriptUrl: 'https://github.com/yurigushiken/eeg-image-analysis/blob/main/eeg_acc%3D1/code/03_generate_n1_contrast_landing_on_6_any_preceding_acc%3D1.py'
+    },
+    {
+        category: 'Topomaps (ACC=1)',
+        subcategory: 'Landing on 6 (Increasing)',
+        dataset: 'ACC=1',
+        name: 'P1',
+        id: 'acc1_p1_land6_increasing',
+        image: 'images/group_p1_contrast_landing_on_6_increasing_acc=1.png',
+        scriptUrl: 'https://github.com/yurigushiken/eeg-image-analysis/blob/main/eeg_acc%3D1/code/03_generate_p1_contrast_landing_on_6_increasing_acc%3D1.py'
+    },
+    {
+        category: 'Topomaps (ACC=1)',
+        subcategory: 'Landing on 6 (Increasing)',
+        dataset: 'ACC=1',
+        name: 'N1',
+        id: 'acc1_n1_land6_increasing',
+        image: 'images/group_n1_contrast_landing_on_6_increasing_acc=1.png',
+        scriptUrl: 'https://github.com/yurigushiken/eeg-image-analysis/blob/main/eeg_acc%3D1/code/03_generate_n1_contrast_landing_on_6_increasing_acc%3D1.py'
+    },
     // Topomaps - ALL
     {
         category: 'Topomaps (ALL)',
@@ -576,6 +612,42 @@ const plotData = [
         id: 'all_n1_land5_increasing',
         image: 'images/group_n1_contrast_landing_on_5_increasing_all.png',
         scriptUrl: 'https://github.com/yurigushiken/eeg-image-analysis/blob/main/eeg_all/code/03_generate_n1_contrast_landing_on_5_increasing_all.py'
+    },
+    {
+        category: 'Topomaps (ALL)',
+        subcategory: 'Landing on 6 (Any Preceding)',
+        dataset: 'ALL',
+        name: 'P1',
+        id: 'all_p1_land6_any',
+        image: 'images/group_p1_contrast_landing_on_6_any_preceding_all.png',
+        scriptUrl: 'https://github.com/yurigushiken/eeg-image-analysis/blob/main/eeg_all/code/03_generate_p1_contrast_landing_on_6_any_preceding_all.py'
+    },
+    {
+        category: 'Topomaps (ALL)',
+        subcategory: 'Landing on 6 (Any Preceding)',
+        dataset: 'ALL',
+        name: 'N1',
+        id: 'all_n1_land6_any',
+        image: 'images/group_n1_contrast_landing_on_6_any_preceding_all.png',
+        scriptUrl: 'https://github.com/yurigushiken/eeg-image-analysis/blob/main/eeg_all/code/03_generate_n1_contrast_landing_on_6_any_preceding_all.py'
+    },
+    {
+        category: 'Topomaps (ALL)',
+        subcategory: 'Landing on 6 (Increasing)',
+        dataset: 'ALL',
+        name: 'P1',
+        id: 'all_p1_land6_increasing',
+        image: 'images/group_p1_contrast_landing_on_6_increasing_all.png',
+        scriptUrl: 'https://github.com/yurigushiken/eeg-image-analysis/blob/main/eeg_all/code/03_generate_p1_contrast_landing_on_6_increasing_all.py'
+    },
+    {
+        category: 'Topomaps (ALL)',
+        subcategory: 'Landing on 6 (Increasing)',
+        dataset: 'ALL',
+        name: 'N1',
+        id: 'all_n1_land6_increasing',
+        image: 'images/group_n1_contrast_landing_on_6_increasing_all.png',
+        scriptUrl: 'https://github.com/yurigushiken/eeg-image-analysis/blob/main/eeg_all/code/03_generate_n1_contrast_landing_on_6_increasing_all.py'
     },
     // P1 vs N1 Analysis
     {
